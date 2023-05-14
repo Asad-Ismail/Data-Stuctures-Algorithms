ffb75dbca6e7e93686099331746a70789b953e24
Asad-Ismail <asadismaeel@gmail.com>
2023-05-14 13:31:50 +0000

    updated graphs

---
+++ graphs/dfs.cpp
@@ -0,0 +1,43 @@
+
+// recursive
+unordered_set<int> seen;
+
+int fn(vector<vector<int>>& graph) {
+    seen.insert(START_NODE);
+    return dfs(START_NODE, graph);
+}
+
+int fn dfs(int node, vector<vector<int>>& graph) {
+    int ans = 0;
+    // do some logic
+    for (int neighbor: graph[node]) {
+        if (seen.find(neighbor) == seen.end()) {
+            seen.insert(neighbor);
+            ans += dfs(neighbor, graph);
+        }
+    }
+
+    return ans;
+}
+
+// Iterative
+
+int fn(vector<vector<int>>& graph) {
+    stack<int> stack;
+    unordered_set<int> seen;
+    stack.push(START_NODE);
+    seen.insert(START_NODE);
+    int ans = 0;
+
+    while (!stack.empty()) {
+        int node = stack.top();
+        stack.pop();
+        // do some logic
+        for (int neighbor: graph[node]) {
+            if (seen.find(neighbor) == seen.end()) {
+                seen.insert(neighbor);
+                stack.push(neighbor);
+            }
+        }
+    }
+}
