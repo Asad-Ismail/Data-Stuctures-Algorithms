5c2ad5f5b12f201dd31e888dce60da81f01a1878
Asad-Ismail <asadismaeel@gmail.com>
2023-05-14 11:38:11 +0000

    updated two input

---
+++ TwoPointers/two_inputs.cpp
@@ -0,0 +1,26 @@
+
+// E.g mergining operation in merge sort 
+int fn(vector<int>& arr1, vector<int>& arr2) {
+    int i = 0, j = 0, ans = 0;
+
+    while (i < arr1.size() && j < arr2.size()) {
+        // do some logic here
+        if (CONDITION) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    while (i < arr1.size()) {
+        // do logic
+        i++;
+    }
+
+    while (j < arr2.size()) {
+        // do logic
+        j++;
+    }
+
+    return ans;
+}
