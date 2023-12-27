02de2706b188d25244752de3172e211b8bf1f427
AsadI <ASADISMAEEL@GMAIL.com>
2023-12-27 21:27:43 +0100

    Update binarySearchTemplate.cpp

--- binarySearch/binarySearchTemplate.cpp
+++ binarySearch/binarySearchTemplate.cpp
@@ -33,3 +33,23 @@ int main() {
 
     return 0;
 }
+
+/// Some examples of above template
+
+bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int low = 1, high = n;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (isBadVersion(mid)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low; // The first bad version.
+}
+
+
+
